4688e561fc9218fb0d21781606db4711022c54fd
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-09-12 16:42:32 +0700

    [Update && Fix] update route category list and fix siderbar UI

---
+++ src/page/admin/category/CategoryList.jsx
@@ -0,0 +1,11 @@
+import CategoryTable from "./CategoryTable"
+import Button from '@mui/material/Button';
+export const CategoryList = () => {
+  return (
+    <>
+      <div className="p-6">
+        <h2 className="text-[44px]">Danh sách danh mục</h2>
+      </div>
+    </>
+  )
+}
