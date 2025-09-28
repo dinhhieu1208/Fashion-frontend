df99fb8b604d64b87a2ea96c58f015f14ded81f1
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-09-28 09:58:02 +0700

    [Update] add pagination in category list

--- src/page/admin/category/CategoryTable.jsx
+++ src/page/admin/category/CategoryTable.jsx
@@ -1,6 +1,7 @@
 import { Edit3, Trash2 } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import { categoriesAdmin } from "@/services/categoryService";
+import PaginationComponent from "@/components/client/Pagination";
 export const CategoryTable = (props) => {
   const { keyword, status } = props;
 
@@ -10,31 +11,35 @@ export const CategoryTable = (props) => {
     retry: false,
   });
 
+  const onChangePage = (pageNumber) => {
+    console.log(pageNumber);
+  }
+
   return (
     <div className="overflow-x-auto ">
       <table className="min-w-full border bg-white  border-gray-200 rounded-lg overflow-hidden shadow-md text-2xl cursor-pointer">
         <thead className="bg-black text-white">
           <tr>
             <th className="px-4 py-2 text-left text-xl font-semibold w-64">
-              Name
+              Tên danh mục
             </th>
             <th className="px-4 py-2 text-left text-xl font-semibold w-32">
-              Images
+              Hình ảnh
             </th>
             <th className="px-4 py-2 text-left text-xl font-semibold w-32">
-              Status
+              Trạng thái
             </th>
             <th className="px-4 py-2 text-left text-xl font-semibold w-40">
-              Created At
+              Ngày tạo
             </th>
             <th className="px-4 py-2 text-left text-xl font-semibold w-40">
-              Created By
+              Người tạo
             </th>
             <th className="px-4 py-2 text-left text-xl font-semibold w-40">
-              Updated By
+              Người sửa
             </th>
             <th className="px-4 py-2 text-center text-xl font-semibold w-40">
-              Action
+              Hành động
             </th>
           </tr>
         </thead>
@@ -53,11 +58,10 @@ export const CategoryTable = (props) => {
                 </td>
                 <td className="px-4 py-2 sm:h-16">
                   <span
-                    className={`inline-flex items-center justify-center px-4 py-2 text-lg font-semibold rounded-full ${
-                      data.status === "active"
+                    className={`inline-flex items-center justify-center px-4 py-2 text-lg font-semibold rounded-full ${data.status === "active"
                         ? "bg-green-100 text-green-700"
                         : "bg-red-100 text-red-700"
-                    }`}
+                      }`}
                   >
                     {data.status}
                   </span>
@@ -83,6 +87,13 @@ export const CategoryTable = (props) => {
             ))}
         </tbody>
       </table>
+
+      <div className="mt-[10px]">
+        <PaginationComponent
+          pages={data?.data?.totalPage || 1}
+          onChangePage={onChangePage}
+        />
+      </div>
     </div>
   );
 };
