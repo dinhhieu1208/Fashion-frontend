bc21a72c4f5fe0e45084ec4b406905a3515c53b2
dinhhieu1208 <nguyenhieu96hth@gmail.com>
2025-11-09 20:53:25 +0700

    [Feature] Trang VoucherList

---
+++ src/page/admin/Voucher/VoucherList.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { VoucherTable } from "./VoucherTable";
+import { Search } from "lucide-react";
+import { useSearchParams } from "react-router-dom";
+
+export const VoucherList = () => {
+  const [keyword, setKeyword] = useState("");
+  const [statusFilter, setStatusFilter] = useState("");
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.search.value);
+    setSearchParams({ search: e.target.search.value, status: statusFilter });
+  };
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setStatusFilter(e.target.value);
+    setSearchParams({ search: keyword, status: e.target.value });
+  };
+
+  return (
+    <div className="p-0 sm:p-4 min-w-screen">
+      {/* Tiêu đề trang */}
+      <div className="mb-[20px] text-[44px] font-[700]">Danh sách voucher</div>
+
+      {/* Thanh tìm kiếm + bộ lọc */}
+      <div className="mb-[20px] flex items-center gap-3">
+        {/* Ô tìm kiếm */}
+        <form
+          action=""
+          className="flex items-center h-[44px] sm:mt-[10px] md:mb-[10px]"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex relative left-[27px]">
+            <Search className="w-[20px] h-[20px] opacity-[0.5]" />
+          </div>
+          <input
+            type="text"
+            placeholder="Tìm kiếm tên voucher..."
+            className="w-full h-full pr-[30px] pl-[40px] py-[20px] rounded-[10px] shadow-md outline-none"
+            name="search"
+          />
+        </form>
+
+        {/* Bộ lọc trạng thái */}
+        <div className="ml-[20px] sm:mt-0 flex items-center">
+          <select
+            defaultValue={statusFilter}
+            className="w-[200px] h-[44px] p-2 bg-white rounded-[10px] shadow-md"
+            onChange={handleOnChange}
+          >
+            <option disabled>Lọc trạng thái</option>
+            <option value="">Tất cả trạng thái</option>
+            <option value="active">Hoạt động</option>
+            <option value="inactive">Dừng hoạt động</option>
+          </select>
+        </div>
+      </div>
+
+      {/* Bảng dữ liệu voucher */}
+      <VoucherTable keyword={keyword} status={statusFilter} />
+    </div>
+  );
+};
