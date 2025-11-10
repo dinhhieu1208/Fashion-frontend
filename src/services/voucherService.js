8cff8181f2cf9dc4790b0181c8d04cbe4cefbfe8
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-11-10 20:10:39 +0700

    [Update] voucher list service

--- src/services/voucherService.js
+++ src/services/voucherService.js
@@ -4,3 +4,9 @@ export const voucherAdd = async (data) => {
   const res = await axiosAdmin.post("/admin/coupon/create", data);
   return res.data
 }
+
+
+export const voucherList = async () => {
+  const res = await axiosAdmin.get("/admin/coupon/list");
+  return res.data
+}
